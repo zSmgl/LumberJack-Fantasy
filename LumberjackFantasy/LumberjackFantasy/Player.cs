ac78c3f04aadf073fb80fa53eb6ddbc28cdb6d38
Marco <jxm5725@rit.edu>
2018-03-10 23:27:10 -0500

    Added AttackState, Type of Attack, and made fixes in in Player Class

--- LumberjackFantasy/LumberjackFantasy/Player.cs
+++ LumberjackFantasy/LumberjackFantasy/Player.cs
@@ -9,6 +9,15 @@ using Microsoft.Xna.Framework.Input;
 
 namespace LumberjackFantasy
 {
+
+    enum AttackVariation                // If we have a sprite sheet class that holds all the varior animations and textures to our game,
+                                        // Axe and Shotgun should be set to the x or y by which the sheet is in, and then be the 2nd num
+                                        // be incremented
+                                        
+    {
+        axe = 10,                       
+        shotgun = 11
+    }
     class Player : LivingObject
     {
         // Jacob Marcovecchio
@@ -18,6 +27,8 @@ namespace LumberjackFantasy
         private int totalScore;         // Player's total score
         private int visionStandard;     // the number that determines how far out the playerVision should be from the actual objectTexture.
         private Rectangle playerVision; // The Players field of vision that helps the bears determine what kind of State they are in.
+        private bool attackStuff;       // Determines if the player can attack something. 
+        AttackVariation attack;         // Enum that determines if the Player should use the shotgun or axe animation when attacking
 
         // Properties -------------------
         public int LevelScore
@@ -28,7 +39,7 @@ namespace LumberjackFantasy
         public int TotalScore
         {
             get { return totalScore; }
-            set { value = totalScore; }
+            set { totalScore = value; }
         }
 
         public Rectangle PlayerVision
@@ -36,6 +47,11 @@ namespace LumberjackFantasy
             get { return playerVision; }
         }
 
+        public AttackVariation SetAttack
+        {
+            get { return attack; }
+            set { attack = value; }
+        }
 
         public Player (int x, int y, int width, int height, Texture2D objectTexture, int maxH, int maxS, int visionStandard)
             : base(x, y, width, height, objectTexture, maxH, maxS)
@@ -44,9 +60,17 @@ namespace LumberjackFantasy
             this.visionStandard = visionStandard;
             playerVision = new Rectangle(x - visionStandard, y - visionStandard, width + (visionStandard * 2), height + (visionStandard * 2));
         }
+        
 
-        public void ProcessPlayerMovementInput(KeyboardState kb)
+        /// <summary>
+        /// Determines both the movement and the attack style, if one, from the player.
+        /// </summary>
+        /// <param name="kb"></param>
+        public void ProcessPlayerInput(KeyboardState kb)
         {
+
+            // Movement & Movement Animation ------------------------------------------
+
             // Up - W
             if (kb.IsKeyDown(Keys.W) == true)
             {
@@ -68,6 +92,79 @@ namespace LumberjackFantasy
                 //objectMovement.addVelocity(?,0)
             }
 
+            // Attack Animation ----------------------------------------
+
+            // The idea is this, if the 
+
+            if (kb.IsKeyDown(Keys.I) == true )
+            {
+                if (attackStuff == false)
+                {
+                    attackStuff = true; 
+                }
+                else
+                {
+                    // Else stuff
+                }
+                /* If the Attack should be over
+                if ( attackStuff = true)
+                    {
+                 attackStuff = false;
+                }
+                */
+            }
+            else if (kb.IsKeyDown(Keys.J) == true)
+            {
+                if (attackStuff == false)
+                {
+                    attackStuff = true;
+                }
+                else
+                {
+                    // Else stuff
+                }
+
+                /* If the Attack should be over
+                if ( attackStuff = true)
+                {
+                    attackStuff = false;
+                }
+                */
+            }
+            else if (kb.IsKeyDown(Keys.J) == true)
+            {
+                if (attackStuff == false)
+                {
+                    attackStuff = true;
+                }
+                else
+                {
+                    // Else stuff
+                }
+                /* If the Attack should be over
+                if ( attackStuff = true)
+                {
+                    attackStuff = false;
+                }
+                */
+            }
+            else if (kb.IsKeyDown(Keys.J) == true)
+            {
+                if (attackStuff == false)
+                {
+                    attackStuff = true;
+                }
+                else
+                {
+                    // Else stuff
+                }
+                /* If the Attack should be over
+                if ( attackStuff = true)
+                {
+                    attackStuff = false;
+                }
+                */
+            }
             // Collisions get handled in the update Position method to assure stuff doesnt end up in other stuff
 
             objectMovement.UpdatePosition(objectCollisionBox);
@@ -85,7 +182,6 @@ namespace LumberjackFantasy
             // Draw method for vision field incase we need to see it
             // This includes sending a texture for the vision texutre. Essentially just a black box.
             // sb.Draw(blackTexture, playerVision, Color.White)
-            base.Draw(sb);
         }
     }
 }
