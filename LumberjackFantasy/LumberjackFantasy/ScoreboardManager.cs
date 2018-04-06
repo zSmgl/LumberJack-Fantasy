12a49f38904bbafb8cf6d8a55adebd86997af892
jtl4027 <jtl4027@IGM2000-20>
2018-04-06 16:05:40 -0400

    Untested gameover code.

--- LumberjackFantasy/LumberjackFantasy/ScoreboardManager.cs
+++ LumberjackFantasy/LumberjackFantasy/ScoreboardManager.cs
@@ -28,144 +28,193 @@ namespace LumberjackFantasy
 		private List<string> scoreNames;    // Holds a list of names tied to high scores
 		private KeyboardState currentKB;    // Holds the current keyboard state
 		private KeyboardState previousKB;   // Holds the last frames keeyboard state
-											
+		bool[] prevPressed;                 // Holda the true false state for each key
+		Vector2[] characterLocations;
+
 		//properties --------------------------------------------------------------------
 		public int CurrentScore { get { return currentScore; } set { currentScore = value; } }
 		//constructor -------------------------------------------------------------------
 		public ScoreboardManager()
 		{
 			scoreState = ScoreState.loading;
+			prevPressed = new bool[256];
+			characterLocations = new Vector2[] { new Vector2(298, 448), new Vector2(448, 448), new Vector2(746, 448) };
 		}
-		//methods -----------------------------------------------------------------------
-		public GameState UpdateGameover() //method for updating the gameover screen
-		{
-			GameState toReturn = GameState.gameOver;
+			//methods -----------------------------------------------------------------------
+			public GameState UpdateGameover() //method for updating the gameover screen
+			{
+				GameState toReturn = GameState.gameOver;
+				currentKB = Keyboard.GetState();
+				switch (scoreState)
+				{
+					case ScoreState.loading:
+						LoadScores();
+						index = TestHighScore();
+						if (index == 10)
+						{
+							scoreState = ScoreState.viewScore;
+						}
+						else
+						{
+							scoreState = ScoreState.getName;
+						}
+
+						break;
+
+					case ScoreState.getName:
+						GetName();
+						scoreNames.Insert(index, playerName);
+						highScores.Insert(index, currentScore);
+						scoreNames.RemoveAt(10);
+						highScores.RemoveAt(10);
+						scoreState = ScoreState.saving;
+						break;
+
+					case ScoreState.saving:
+						SaveScores();
+						scoreState = ScoreState.viewScore;
+						break;
+
+					case ScoreState.viewScore:
+						if (currentKB.IsKeyDown(Keys.Enter))
+						{
+							toReturn = GameState.start;
+						}
+						break;
+
+				}
+				previousKB = currentKB;
+				return toReturn;
+			}
+
+			public void DrawGameOver(SpriteBatch spriteBatch, SpriteFont spriteFont) //method that draws the gameover screen
+			{
 			switch (scoreState)
 			{
 				case ScoreState.loading:
-					LoadScores();
-					index = TestHighScore();
-					if (index == 10)
-					{
-						scoreState = ScoreState.viewScore;
-					}
-					else
-					{
-						scoreState = ScoreState.getName;
-					}
-
 					break;
 
 				case ScoreState.getName:
-					//get the players name
-					scoreNames.Insert(index, playerName);
-					highScores.Insert(index, currentScore);
-					scoreNames.RemoveAt(10);
-					highScores.RemoveAt(10);
-					scoreState = ScoreState.saving;
-					break;
-
-				case ScoreState.saving:
-					SaveScores();
-					scoreState = ScoreState.viewScore;
-					break;
-
-				case ScoreState.viewScore:
-					if (currentKB.IsKeyDown(Keys.Enter))
+					if (playerName != null)
 					{
-						toReturn = GameState.start;
+						if (playerName.Length == 1)
+						{
+							spriteBatch.DrawString(spriteFont, playerName[0].ToString(), characterLocations[0], Color.Firebrick);
+						}
+						if (playerName.Length == 2)
+						{
+							spriteBatch.DrawString(spriteFont, playerName[1].ToString(), characterLocations[1], Color.Firebrick);
+						}
+						if (playerName.Length == 3)
+						{
+							spriteBatch.DrawString(spriteFont, playerName[2].ToString(), characterLocations[2], Color.Firebrick);
+						}
 					}
 					break;
 
-			}
-			return toReturn;
-		}
-
-		public void DrawGameOver(SpriteBatch spriteBatch) //method that draws the gameover screen
-		{
-
-		}
-
-		public void LoadScores() //method to load in the current high score list
-		{
-			StreamReader toLoad = new StreamReader("High Score.txt");
-			try
-			{
-				string line = null;
-				while ((line = toLoad.ReadLine()) != null)
-				{
-					string[] split = line.Split(',');
-					scoreNames.Add(split[0]);
-					highScores.Add(Int32.Parse(split[1]));
-				}
-			}
-
-			catch (Exception e)
-			{
-				Console.WriteLine("Error loading: " + e.Message + "\n");
-			}
-			finally
-			{
-				toLoad.Close();
-			}
-		}
-
-		public void SaveScores()
-		{
-			StreamWriter saveScore = new StreamWriter("High Score.txt");
-			try
-			{
-				for (int count = 0; count < 10; count++)
-				{
-					saveScore.WriteLine(scoreNames[count] + "," + highScores[count]);
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("Error saving: " + e.Message + "\n");
-			}
-			finally
-			{
-				saveScore.Close();
-			}
-		}
-
-		public int TestHighScore() //method to see where to place the new score in on a return of 10 it is not high enough.
-		{
-			int count = 0;
-			foreach (int thisScore in highScores)
-			{
-				if (currentScore > thisScore)
-				{
+				case ScoreState.saving:
 					break;
+
+				case ScoreState.viewScore:
+					break;
+			}
+			}
+
+			public void LoadScores() //method to load in the current high score list
+			{
+				StreamReader toLoad = new StreamReader("High Score.txt");
+				try
+				{
+					string line = null;
+					while ((line = toLoad.ReadLine()) != null)
+					{
+						string[] split = line.Split(',');
+						scoreNames.Add(split[0]);
+						highScores.Add(Int32.Parse(split[1]));
+					}
 				}
-				count++;
+
+				catch (Exception e)
+				{
+					Console.WriteLine("Error loading: " + e.Message + "\n");
+				}
+				finally
+				{
+					toLoad.Close();
+				}
 			}
 
-			return count;
-		}
-
-		public void GetName() //method that saves players name
-		{
-			int count = 0;
-			while (count < 3)
+			public void SaveScores()
 			{
-				//put code here to input name
+				StreamWriter saveScore = new StreamWriter("High Score.txt");
+				try
+				{
+					for (int count = 0; count < 10; count++)
+					{
+						saveScore.WriteLine(scoreNames[count] + "," + highScores[count]);
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("Error saving: " + e.Message + "\n");
+				}
+				finally
+				{
+					saveScore.Close();
+				}
 			}
-	
-		}
-	
 
-
-		public bool SinglePress(Keys key)
-		{
-			if (currentKB.IsKeyDown(key) == true && (previousKB.IsKeyDown(key) != true))
+			public int TestHighScore() //method to see where to place the new score in on a return of 10 it is not high enough.
 			{
-				return true;
-			}
-			return false;
-		}
+				int count = 0;
+				foreach (int thisScore in highScores)
+				{
+					if (currentScore > thisScore)
+					{
+						break;
+					}
+					count++;
+				}
 
+				return count;
+			}
+
+			public void GetName() //method that saves players name
+			{
+				while (playerName.Length < 3)
+				{
+					Keys[] currentPressed = currentKB.GetPressedKeys();
+					foreach (Keys key in currentPressed)
+					{
+						if (prevPressed[(int)key] == false && key >= Keys.A && key <= Keys.Z)
+						{
+							char inputKey = (char)key;
+							if (playerName == null)
+							{
+								playerName = inputKey.ToString();
+							}
+							else
+							{
+								playerName = playerName + inputKey.ToString();
+							}
+						}
+					}
+				}
+
+			}
+
+
+
+			public bool SinglePress(Keys key)
+			{
+				if (currentKB.IsKeyDown(key) == true && (previousKB.IsKeyDown(key) != true))
+				{
+					return true;
+				}
+				return false;
+			}
+
+		}
 	}
-}
+
 
