bed39515b03e97534b10e90352947c815719971e
jxd4609 <jxd4609@jxd4609@rit.edu>
2018-04-11 17:01:36 -0400

    Edit OpenSeason Class

--- LumberjackFantasy/LumberjackFantasy/OpenSeasonManager.cs
+++ LumberjackFantasy/LumberjackFantasy/OpenSeasonManager.cs
@@ -14,45 +14,68 @@ namespace LumberjackFantasy
         private double openSeasonTimer;
         private double openSeasonLimiter;
         private bool openSeason;
-        private Camera camera;
+        private int bearMax;
 
         //Constructor -------------------------------------------
         private OpenSeasonManager()
         {
+            openSeason = true;
             openSeasonTimer = 60;
             openSeasonLimiter = 0;
-            camera = new Camera(0,camera.CamTexture);
         }
 
         //Methods ------------------------------------------------
-        public void SetOpenSeason(List<Bear> bearscurrent)
+        public List<Bear> SetOpenSeasonList(List<Bear> bearscurrent)
         {
             for(int i=0;i<bearscurrent.Count;i++)
             {
                 bearscurrent[i].MaxSpeed = bearscurrent[i].MaxSpeed * 2;
                 bearscurrent[i].VisionStandard = bearscurrent[i].VisionStandard + 10;
-                camera.BearMax = bearscurrent[i].MaxSpeed;
+                bearscurrent[i].BearVision = new Rectangle(bearscurrent[i].BearVision.X - bearscurrent[i].VisionStandard , bearscurrent[i].BearVision.Y - bearscurrent[i].VisionStandard,
+                bearscurrent[i].BearVision.Width + (bearscurrent[i].VisionStandard *2) , bearscurrent[i].BearVision.Height + (bearscurrent[i].VisionStandard * 2));
             }
+            bearMax = bearscurrent[0].MaxSpeed;
+            return bearscurrent;
         }
 
-        public void EndOpenSeason(List<Bear> bearscurrent, Player pcurrent)
+        public Camera OpenSeasonCamera(Camera camera)
+        {
+            camera.BearMax = bearMax;
+            return camera;
+        }
+
+        public Player SetOpenSeasonPlayer(Player pcurrent)
+        {
+            pcurrent.Attack = AttackVariation.shotgun;
+            return pcurrent;
+        }
+
+        public List<Bear> EndOpenSeasonList(List<Bear> bearscurrent, Player pcurrent)
         {
             for (int i = 0; i < bearscurrent.Count; i++)
             {
                 bearscurrent[i].MaxSpeed = bearscurrent[i].MaxSpeed * 1 / 2;
                 bearscurrent[i].VisionStandard = bearscurrent[i].VisionStandard - 10;
-                camera.BearMax = bearscurrent[i].MaxSpeed;
-                pcurrent.Attack = AttackVariation.axe;
+                bearscurrent[i].BearVision = new Rectangle(bearscurrent[i].BearVision.X + bearscurrent[i].VisionStandard, bearscurrent[i].BearVision.Y + bearscurrent[i].VisionStandard,
+                bearscurrent[i].BearVision.Width - (bearscurrent[i].VisionStandard * 2), bearscurrent[i].BearVision.Height - (bearscurrent[i].VisionStandard * 2));
             }
+            bearMax = bearscurrent[0].MaxSpeed;
+            return bearscurrent;
         }
 
+        public Player EndOpenSeasonPlayer(Player pcurrent)
+        {
+            pcurrent.Attack = AttackVariation.axe;
+            return pcurrent;
+        }
         public void UpdateTimer(GameTime gameTime)
         {
-            UpdateTimer(gameTime);
+            openSeasonTimer -= gameTime.ElapsedGameTime.TotalSeconds;
         }
 
         public bool OpenSeason()
         {
+            openSeason = true;
             if(openSeasonTimer == openSeasonLimiter)
             {
                 openSeason = false;
