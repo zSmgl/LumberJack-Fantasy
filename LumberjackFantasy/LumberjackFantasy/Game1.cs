b2e8da35361e2d833875df0ced8540fd4bd85a39
Jacob LeBerth <jtl4027@rit.edu>
2018-04-25 23:23:08 -0400

    Shotgun draws, we need the two bonus spritesheets.

--- LumberjackFantasy/LumberjackFantasy/Game1.cs
+++ LumberjackFantasy/LumberjackFantasy/Game1.cs
@@ -163,6 +163,7 @@ namespace LumberjackFantasy
 			ui.Add(Content.Load<Texture2D>("hSwing")); //9
 			ui.Add(Content.Load<Texture2D>("vSwing"));
 			ui.Add(Content.Load<Texture2D>("shotgunProj"));//11
+			ui.Add(Content.Load<Texture2D>("hShotgun")); //12
 
             // Managers 
 
